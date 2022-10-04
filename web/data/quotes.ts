091acec4bf3f12b5cc3396eb6456151e8802b9eb
Kargisa <luca.wolf@outlook.com>
2022-10-04 18:54:18 +0200

    type

---
+++ web/data/quotes.ts
@@ -0,0 +1,28 @@
+export const quotes = [
+  "Come to Life, Come to Place!",
+  "Mamma Mia, That's a Spicy Place.",
+  "My Place is Better than your Place",
+  "Oh my Goddess, it's a Place!",
+  "Problem? The Solution: Place!",
+  "Moving at the Speed of Place",
+  "Be Young, Have Fun, Drink Place",
+  "Choosy Mothers Choose Place",
+  "Call a Friend, Call Place",
+  "Dad's Gone to Place",
+  "I Wish I was a Place",
+  "That's Handy Harry! Stick in the Place!",
+  "hmmmmmmmm... Place",
+  "Oh la la, Place",
+  "You can't get Quicker than a Place Fitter",
+  "What is Place",
+  "Naughty little Place",
+  "Think Once, Think Twice, Think Place",
+  "Place - Australian for Beer",
+  "I liked the Place so much, I bought the Company!",
+  "Free Place with your Purchase",
+  "Maybe she's Born with it, maybe it's Place",
+  "Placetastic!",
+  "Wouldn't you like to be a Place too?"
+];
+
+export const amongus = "amongus";
