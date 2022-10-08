aea8e157a13371f339de8190449c871dd61c0fc2
Jan540 <naj.schaefer@gmail.com>
2022-10-08 11:52:55 +0200

    validate user registration 👽

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -12,23 +12,8 @@ export class UserResolver {
 
   @Mutation(() => UserResponse)
   async register(@Arg("options") options: UsernamePasswordInput) {
-    if (options.username.length <= 2) {
-      return [
-        {
-          field: "username",
-          message: "username too short :( --> must be greater than 2",
-        },
-      ];
-    }
-
-    if (options.password.length <= 2) {
-      return [
-        {
-          field: "password",
-          message: "password too short :( --> must be greater than 2",
-        },
-      ];
-    }
+    const errors = await validateNewUser(options.email, options.username, options.password);
+    if (errors) return { errors };
 
     const hashedPassword = await bcrypt.hash(options.password, 10);
 
@@ -42,4 +27,67 @@ export class UserResolver {
 
     return { user };
   }
+}
+
+async function validateNewUser(email: string, username: string, password: string) {
+  // http://emailregex.com/
+  const emailRegex: RegExp =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  // at least 3 characters long, no _ or . at the beginning / end, no __ or _. or ._ or .. inside
+  const usernameRegex: RegExp = /^(?=.{3,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/;
+
+  // Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character
+  const passwordRegex: RegExp =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+  const userEmail = await User.findOne({ where: { email } });
+  const userName = await User.findOne({ where: { username } });
+
+  if (userEmail) {
+    return [
+      {
+        field: "email",
+        message: "email already taken",
+      },
+    ];
+  }
+
+  if (!emailRegex.test(email)) {
+    return [
+      {
+        field: "email",
+        message: "invalid email",
+      },
+    ];
+  }
+
+  if (userName) {
+    return [
+      {
+        field: "username",
+        message: "username already taken",
+      },
+    ];
+  }
+
+  if (!usernameRegex.test(username)) {
+    return [
+      {
+        field: "username",
+        message: `username does not meet requirements`,
+      },
+    ];
+  }
+
+  if (!passwordRegex.test(password)) {
+    return [
+      {
+        field: "password",
+        message: `password does not meet requirements`,
+      },
+    ];
+  }
+
+  return null;
 }
