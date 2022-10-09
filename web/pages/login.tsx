3a051d846a1d315c6eca353ec26d763b1401612c
Tobias <tobiast2004@outlook.de>
2022-10-09 16:37:26 +0000

    hello

--- web/pages/login.tsx
+++ web/pages/login.tsx
@@ -1,7 +1,51 @@
+import {
+  Flex,
+  Text,
+  Input,
+  Stack,
+  InputRightElement,
+  InputGroup,
+  Button,
+} from "@chakra-ui/react";
 import { NextPage } from "next";
+import React from "react";
+import { FC } from "react";
 
 const Login: NextPage = () => {
-  return <h1>login</h1>;
+  return (
+    <>
+      <Flex justifyContent="center" alignItems="center" height="50vh">
+        <Text color="red" fontSize="6xl">
+          Login
+        </Text>
+      </Flex>
+      <Flex justifyContent="center" height="50vh">
+        <LoginFields />
+      </Flex>
+    </>
+  );
+};
+
+const LoginFields: FC = () => {
+  const [show, setShow] = React.useState(false);
+  const handleClick = () => setShow(!show);
+  return (
+    <Stack spacing="3">
+      <Input width="6xl" placeholder="Username" />
+      <InputGroup>
+        <Input
+          width="6xl"
+          placeholder="Password"
+          type={show ? "text" : "Password"}
+        />
+        <InputRightElement width="4.5rem">
+          <Button h="1.75rem" size="sm" onClick={handleClick}>
+            {show ? "Hide" : "Show"}
+          </Button>
+        </InputRightElement>
+      </InputGroup>
+    </Stack>
+  );
 };
 
 export default Login;
