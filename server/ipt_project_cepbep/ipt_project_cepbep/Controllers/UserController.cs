5c622178674da8f231d4d274bd3aca0fad51a31e
Jan540 <naj.schaefer@gmail.com>
2022-10-19 12:32:57 +0200

    commit message

---
+++ server/ipt_project_cepbep/ipt_project_cepbep/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+using ipt_project_cepbep.Data;
+using ipt_project_cepbep.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ipt_project_cepbep.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UserController : ControllerBase
+{
+    private AppDbContext _context;
+    
+    public UserController(IConfiguration configuration)
+    {
+        _context = new AppDbContext(configuration);
+    }
+
+    [HttpGet("{username}")]
+    public IEnumerable<User> GetUser(string username)
+    {
+        return _context.Users.Where(u => u.Username == username);
+    }
+}
--- server/ipt_project_cepbep/ipt_project_cepbep/Models/User.cs
+++ server/ipt_project_cepbep/ipt_project_cepbep/Models/User.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 using Microsoft.EntityFrameworkCore;
 
 namespace ipt_project_cepbep.Models;
@@ -11,5 +12,6 @@ public class User : BaseModel
     public Guid UserId { get; set; }
     public string Email { get; set; }
     public string Username { get; set; }
+    [JsonIgnore]
     public string Password { get; set; }
 }
