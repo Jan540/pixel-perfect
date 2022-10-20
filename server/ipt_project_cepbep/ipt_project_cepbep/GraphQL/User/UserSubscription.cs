2fafab6ecf0f0e997e73c982e26379baf03a9908
Jan540 <naj.schaefer@gmail.com>
2022-10-20 12:34:52 +0200

    ökjölkjölkj 🌝

--- server/ipt_project_cepbep/ipt_project_cepbep/GraphQL/User/UserSubscription.cs
+++ server/ipt_project_cepbep/ipt_project_cepbep/GraphQL/User/UserSubscription.cs
@@ -5,5 +5,5 @@ namespace ipt_project_cepbep.GraphQL.User;
 public class UserSubscription
 {
     [Subscribe]
-    public Models.User OnUserCreation([EventMessage] Models.User user) => user;
+    public User OnUserCreation([EventMessage] Models.User user) => user;
 }
