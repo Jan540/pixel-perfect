d40f18a4a3dc2af9164d22687a443a8a5fcf578a
Kargisa <luca.wolf@outlook.com>
2022-12-21 12:13:25 +0100

    Bogus Amogus

--- web/pages/privatePlace.tsx
+++ web/pages/privatePlace.tsx
@@ -1,15 +1,10 @@
-import GlobalStyles from "../assets/GlobalStyles";
-import DrawCanvas from "../components/DrawCanvas/DrawCanvas";
 import { NextPage } from "next";
-import { Flex } from "@chakra-ui/react";
+import { Flex, Text } from "@chakra-ui/react";
 
 const privatePlace: NextPage = () => {
   return (
     <>
-      <GlobalStyles />
-      <Flex alignContent="center" justifyContent="center">
-        <DrawCanvas w={30} h={15} />
-      </Flex>
+      <Text size={"xl"}>AMOGUS</Text>
     </>
   );
 };
