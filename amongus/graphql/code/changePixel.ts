180a13f119081ac7c4c469823b03a6fa9aa8a2d4
Jan Schäfer <naj.schaefer@gmail.com>
2023-01-31 18:18:46 +0000

    aksjfhslkjdfhs🌠

--- amongus/graphql/code/changePixel.ts
+++
@@ -1,9 +0,0 @@
-import { graphql } from "../generated/gql";
-
-export const CHANGE_PIXEL = graphql(/* GraphQL */ `
-  mutation changePixel($input: ChangePixelColorInput!) {
-    changePixelColor(input: $input) {
-      boolean
-    }
-  }
-`);
